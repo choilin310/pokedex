276ada3d735af97a8186863b84ba6cb3ed45679e
choilin310 <choiinhwan0@gmail.com>
2023-08-01 18:06:08 -0400

    started pokedex

---
+++ pokedex/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import pokeball from "../images/pokeball.png";
+import pokeballcolor from "../images/pokeballcolor.png"
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="title">
+      <div className="title__left">
+        <p>Pokédex</p>
+        <div className="caught__seen">
+          <div className="caught">
+            <img src={pokeball} alt="pokeball" className="caught__pokeball" />
+            <p>438</p>
+          </div>
+          <div className="seen">
+            <img src={pokeballcolor} alt="pokeball" className="seen__pokeball"/>
+            <p>649</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
